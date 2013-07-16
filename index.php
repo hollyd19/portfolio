0bdabfafe600d858c585e9ee062736f2f8e66f12
Holly Domke <hed35@cornell.edu>
2013-07-15 23:41:04 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href='http://fonts.googleapis.com/css?family=Pompiere' rel='stylesheet' type='text/css'>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="javascript.js"></script>
+<title>Holly Domke</title>
+</head>
+<body>
+<div id="filler_left"></div>
+<div id="filler_top"></div>
+
+<div id="name_header" class="content">
+<h1 id="name">Holly E. Domke</h1>
+</div>
+
+<div id="intro_content" class="content active">
+<h1 id="name">Holly E. Domke</h1>
+<ul id="desc">
+	<li>Cornell University Student</li>
+	<li>User Experience Designer</li>
+</ul>
+<br/>
+<q>I don't think technology should be scary.</q>
+</div>
+
+<div id="navigation">
+	<div class="nav_link" id="about_me_link">
+		<p>About Me</p>
+	</div>
+	<div class="nav_link" id="experience_link">
+		<p>Experience</p>
+	</div>
+	<div class="nav_link" id="links_link">
+		<p>Links</p>
+	</div>
+</div>
+
+<div id="about_me" class="content">
+<h1 class="title">About Me<h1>
+</div>
+
+<div id="experience" class="content">
+<h1 class="title">Experience<h1>
+</div>
+
+<div id="links" class="content">
+<h1 class="title">Links<h1>
+</div>
+
+
+</body>
+</html>
---
+++ javascript.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+	$('#intro_content').show();
+	$('#name_header').hide();
+	$('#about_me').hide();
+	$('#experience').hide();
+	$('#links').hide();
+	
+	$('#about_me_link').hover(function(){
+		$(this).css('color', '#EDD1B7');
+		$(this).css('font-weight', 'bold');
+		
+	}, function() {
+		$(this).css('color', '');
+		$(this).css('font-weight', '');
+	})
+	
+	$('#experience_link').hover(function(){
+		$(this).css('color', '#EDD1B7');
+		$(this).css('font-weight', 'bold');
+		
+	}, function() {
+		$(this).css('color', '');
+		$(this).css('font-weight', '');
+	})
+	
+	$('#links_link').hover(function(){
+		$(this).css('color', '#EDD1B7');
+		$(this).css('font-weight', 'bold');
+		
+	}, function() {
+		$(this).css('color', '');
+		$(this).css('font-weight', '');
+	})
+	
+	$('#name_header').hover(function(){
+		$(this).css('color', '#EDD1B7');
+		$(this).css('font-weight', 'bold');
+		
+	}, function() {
+		$(this).css('color', '');
+		$(this).css('font-weight', '');	
+	})
+	
+	$('#about_me_link').click(function(){
+		$('.active').hide();
+		$('.active').removeClass('active');
+		$('.current').removeClass('current');
+		$('#name_header').show();
+		$('#filler_top').css('height','10%');
+		$('#about_me').show();
+		$('#about_me').addClass('active');
+		$('#about_me_link').addClass('current');
+	})
+	
+	$('#experience_link').click(function(){
+		$('.active').hide();
+		$('.active').removeClass('active');
+		$('.current').removeClass('current');
+		$('#name_header').show();
+		$('#filler_top').css('height','10%');
+		$('#experience').show();
+		$('#experience').addClass('active');
+		$('#experience_link').addClass('current');
+	})
+	
+	$('#links_link').click(function(){
+		$('.active').hide();
+		$('.active').removeClass('active');
+		$('.current').removeClass('current');
+		$('#name_header').show();
+		$('#filler_top').css('height','10%');
+		$('#links').show();
+		$('#links').addClass('active');
+		$('#links_link').addClass('current');
+	})
+	
+	$('#name_header').click(function(){
+		$('.active').hide();
+		$('.active').removeClass('active');
+		$('.current').removeClass('current');
+		$('#name_header').hide();
+		$('#filler_top').css('height','20%');
+		$('#intro_content').show();
+		$('#intro_content').addClass('active');
+	})
+	
+  
+});
